13bca95580b8b0c3a3a75970cf229a0a30bda96f
Ashley Coles <ashley.coles@bosonweb.net>
2018-05-14 23:25:02 +0100

    adding support for future bootstrap layouts plugin

--- acf_layouts.php
+++ acf_layouts.php
@@ -10,11 +10,18 @@ Author: Ashley Coles
 require_once('acf_layouts_create_fields.php');
 
 function load_layouts($content_field) {
-	if( have_rows($content_field) ):					    
+	if( have_rows($content_field) ):
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		while ( have_rows($content_field) ) : the_row();	    
 			$layout = get_row_layout();
 			// check for and set a template in the theme
 			$theme_template_path = (file_exists(get_stylesheet_directory() . '/layouts/layout-' . $layout . '.php') ? get_stylesheet_directory() . '/layouts/layout-' . $layout . '.php' : null);
+			$bootstrap_template_path = null;
+			// check for plugin using plugin name
+			if ( is_plugin_active( 'acf_layouts_bootstrap_templates/acf_layouts_bootstrap_templates.php' ) ) {
+			  	// check for and set a template in the theme
+				$bootstrap_template_path = (file_exists(plugin_dir_path(__FILE__) . '../acf_layouts_bootstrap_templates/layouts/layout-' . $layout . '.php') ? plugin_dir_path(__FILE__) . '../acf_layouts_bootstrap_templates/layouts/layout-' . $layout . '.php' : null);
+			} 
 			// check for and set a template in this plugin
 			$internal_template_path = (file_exists(plugin_dir_path(__FILE__) . 'layouts/layout-' . $layout . '.php') ? plugin_dir_path(__FILE__) . 'layouts/layout-' . $layout . '.php' : null);
 			// have we loaded a template file?
@@ -24,6 +31,11 @@ function load_layouts($content_field) {
 				include($theme_template_path);
 				$template_loaded = true;
 			} 
+			// Do we have a bootstrap template?
+			else if ( $bootstrap_template_path ) {
+				include($bootstrap_template_path);
+				$template_loaded = true;
+			}
 			// then try within this plugin
 			else if ( $internal_template_path && !$template_loaded ) {
 				include($internal_template_path);
--- acf_layouts_create_fields.php
+++ acf_layouts_create_fields.php
@@ -1,6 +1,6 @@
 <?php
 
-if( function_exists('acf_add_local_field_group') ):
+if ( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
 	'key' => 'group_5af6f62a6271e',
